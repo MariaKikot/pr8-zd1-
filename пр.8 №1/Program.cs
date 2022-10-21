b601a7df33c0c2e2c30330a82cace53ef077d1cb
Виталий <@DESKTOP-NTOVGEQ>
2022-10-22 01:37:19 +0300

    Добавьте файлы проекта.

---
+++ пр.8 №1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение х=");
+            double x = double.Parse(Console.ReadLine());
+            double y;
+
+            if (x > 2.8 && x < 6)
+                y = (x - 1) / (x + 1);
+            else
+            {
+                if (x > 6)
+                    y = (Math.Pow(Math.E, x)) + (Math.Sin(x));
+                else
+                {
+                    y = (x);
+                }
+            }
+            Console.WriteLine("y=" + y);
+
+        }
+    }
+}
